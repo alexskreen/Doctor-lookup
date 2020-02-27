b8d9b84de920630fd9801a7a5116098069914f81
Alex Skreen <skreenalex@gmail.com>
2020-02-27 14:49:51 -0800

    updated api call in doctor-service.js

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -2,7 +2,7 @@ export class DoctorService {
   async getDoctorByIssue(issue) {
     try {
       let response = await fetch(
-        `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.5051%2C-122.6750%2C100&user_location=45.5051%2C-122.6750&skip=0&limit=10&user_key=API_KEY`
+        `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.5051%2C-122.6750%2C100&user_location=45.5051%2C-122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`
       );
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
@@ -20,7 +20,7 @@ export class DoctorService {
   async getDoctorByName(name) {
     try {
       let response = await fetch(
-        `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5051%2C-122.6750%2C100&user_location=45.5051%2C-122.6750&skip=0&limit=10&user_key=API_KEY`
+        `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5051%2C-122.6750%2C100&user_location=45.5051%2C-122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`
       );
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
